60525c28a4c47fd7af018a79a0cf5cbceaabf6d6
Timothy Shea <tshea31@gmail.com>
2025-11-02 10:40:14 -0700

    r - remove encryption version from File table

---
+++ Data/Migrations/20251102173735_Remove_EncryptionFormatVersion_From_File.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lockhaven_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class Remove_EncryptionFormatVersion_From_File : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncryptionFormatVersion",
+                table: "Files");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EncryptionFormatVersion",
+                table: "Files",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 1);
+        }
+    }
+}
